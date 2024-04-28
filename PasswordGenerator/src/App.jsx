04a2a695fad308f5433c57c7cc45bf3a654574fa
DHARMENDRA PRAJAPATI <dharmendraprajapati9503@gmail.com>
2024-04-28 10:25:05 +0530

    Password Generator project

---
+++ PasswordGenerator/src/App.jsx
@@ -0,0 +1,117 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+function App() {
+
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordRef = useRef(null);
+
+  /*
+  * Function for generate random password
+  *
+  * 
+  * Here we are giving dependencies to optimize the function and avoid unnecessary re-renders.
+  * 
+  * useCallback is about memoizing a function, while useEffect is about running side effects after 
+  * rendering and when dependencies change.
+  * 
+  * Here dependencies used to optimize method and avoid unnecessary re-renders.
+  */
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (characterAllowed) str += "!@#$%^&*_-+={}[]~`";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, characterAllowed, setPassword]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, characterAllowed, passwordGenerator]);
+
+  return (
+    <div className="w-full h-screen bg-black">
+      <div className='flex flex-col gap-3 justify-center items-center py-10 text-orange-500'>
+        <h1 className="text-4xl text-center text-white">Password Generator</h1>
+        <div className="flex flex-col w-2/6 min-w-80 bg-gray-500 gap-4 py-6 my-4 px-6 items-center rounded-lg">
+          <div id="generate-copy-password" className="w-full max-w-sm">
+            <div className="flex items-center">
+              <button
+                onClick={passwordGenerator}
+                className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-2 text-sm font-medium text-center text-white bg-blue-700 dark:bg-blue-600 border dark:hover:bg-blue-700 rounded-s-lg border-blue-700 dark:border-blue-600 hover:border-blue-700 dark:hover:border-blue-700 focus:ring-1 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 hover:bg-blue-200">
+                Generate
+              </button>
+              <div className="relative w-full">
+                <input id="password-generator"
+                  type="text"
+                  className="bg-gray-50 text-sm block w-full p-2.5 text-orange-500 outline-none"
+                  value={password}
+                  readOnly
+                  ref={passwordRef}
+                />
+              </div>
+              <button onClick={copyPasswordToClipboard} className="flex-shrink-0 z-10 inline-flex items-center py-3 px-4 text-sm font-medium text-center text-gray-500 dark:text-gray-400 hover:text-gray-900 bg-gray-100 border border-gray-300 rounded-e-lg hover:bg-gray-200 focus:ring-1 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:hover:text-white dark:border-gray-600" type="button">
+                <span id="default-icon">
+                  <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                    <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
+                  </svg>
+                </span>
+              </button>
+            </div>
+          </div>
+          <div className='flex text-sm gap-x-2'>
+            <div className='flex items-center gap-x-1'>
+              <input
+                type="range"
+                min={6}
+                max={50}
+                value={length}
+                className='cursor-pointer'
+                onChange={(e) => { setLength(e.target.value) }}
+              />
+              <label>Length: {length}</label>
+            </div>
+            <div className="flex items-center gap-x-1">
+              <input
+                type="checkbox"
+                defaultChecked={numberAllowed}
+                id="numberInput"
+                onChange={() => {
+                  setNumberAllowed((prev) => !prev);
+                }}
+              />
+              <label htmlFor="numberInput">Numbers</label>
+            </div>
+            <div className="flex items-center gap-x-1">
+              <input
+                type="checkbox"
+                defaultChecked={characterAllowed}
+                id="characterInput"
+                onChange={() => {
+                  setCharacterAllowed((prev) => !prev)
+                }}
+              />
+              <label htmlFor="characterInput">Characters</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div >
+  )
+}
+
+export default App
