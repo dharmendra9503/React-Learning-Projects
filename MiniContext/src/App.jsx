9d12c012f579e2fa19f905e3bae40cba111495ea
DHARMENDRA PRAJAPATI <dharmendraprajapati9503@gmail.com>
2024-05-01 12:25:54 +0530

    Project to learn context API

---
+++ MiniContext/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
