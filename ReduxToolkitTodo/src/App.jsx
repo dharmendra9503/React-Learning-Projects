2cbb1f85e7f113e6de72709cb39937d2de5751dc
DHARMENDRA PRAJAPATI <dharmendraprajapati9503@gmail.com>
2024-05-05 18:37:04 +0530

    Reduc Toolkit

---
+++ ReduxToolkitTodo/src/App.jsx
@@ -0,0 +1,13 @@
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
